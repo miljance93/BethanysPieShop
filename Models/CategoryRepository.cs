c4a9cf3b37dcef28686ccce45d9b4161d6795781
Miljan <miljanpjanovic@gmail.com>
2022-03-16 17:58:21 +0100

    Initial Commit

---
+++ Models/CategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly AppDbContext context;
+
+        public CategoryRepository(AppDbContext context)
+        {
+            this.context = context;
+        }
+        public IEnumerable<Category> AllCategories => context.Categories;
+    }
+}
