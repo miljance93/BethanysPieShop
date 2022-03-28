510a569a2d61edd0df85ff34141100b0ac0e5bf6
Miljan <miljanpjanovic@gmail.com>
2022-03-28 06:34:33 +0200

    MS-2 Create "Pie Manager" page

--- BethanysPieShop/Controllers/PieManagerController.cs
+++ BethanysPieShop/Controllers/PieManagerController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,36 @@ namespace BethanysPieShop.Controllers
 {
     public class PieManagerController : Controller
     {
-        public IActionResult Index()
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieManagerController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
         {
-            return View();
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+        public ViewResult List(string category)
+        {
+            IEnumerable<Pie> pies;
+            string currentCategory;
+
+            if (string.IsNullOrEmpty(category))
+            {
+                pies = _pieRepository.AllPies.OrderBy(p => p.PieId);
+                currentCategory = "All pies";
+            }
+            else
+            {
+                pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName == category)
+                    .OrderBy(p => p.PieId);
+                currentCategory = _categoryRepository.AllCategories.FirstOrDefault(c => c.CategoryName == category)?.CategoryName;
+            }
+
+            return View(new PiesListViewModel
+            {
+                Pies = pies,
+                CurrentCategory = currentCategory
+            });
         }
     }
 }
