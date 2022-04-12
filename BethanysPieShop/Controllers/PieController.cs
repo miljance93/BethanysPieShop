944b46aa5a3ab6613a46c15a8c7439fb2f9b94d7
Miljan <miljanpjanovic@gmail.com>
2022-04-12 20:15:29 +0200

    MS-14 Created filter by name functioanlity

--- BethanysPieShop/Controllers/PieController.cs
+++ BethanysPieShop/Controllers/PieController.cs
@@ -84,29 +84,27 @@ namespace BethanysPieShop.Controllers
             return View(vm);
         }
 
-        public ViewResult FilterCategory(int categoryId)
+        public ViewResult FilterCategory(int categoryId, string pieName)
         {
             IEnumerable<Pie> pies;
             int currentCategoryId = 0;
 
-            if (categoryId == 0)
+            if (categoryId == 0 || string.IsNullOrEmpty(pieName))
             {
                 pies = _pieRepository.AllPies.OrderBy(p => p.PieId);
             }
 
             else
             {
-                pies = _pieRepository.AllPies.Where(p => p.Category.CategoryId == categoryId)
+                pies = _pieRepository.AllPies.Where(p => p.Name == pieName && p.Category.CategoryId == categoryId)
                     .OrderBy(p => p.PieId);
                 currentCategoryId = _categoryRepository.AllCategories.FirstOrDefault(c => c.CategoryId == categoryId).CategoryId;
             }
-            
+
 
             return View(new PiesListViewModel()
             {
-                Pies = pies,
-                CategoryId = currentCategoryId,
-                Categories = _categoryRepository.AllCategories
+                Pies = pies
             });
         }
     }
--- BethanysPieShop/ViewModels/PiesListViewModel.cs
+++ BethanysPieShop/ViewModels/PiesListViewModel.cs
@@ -6,6 +6,7 @@ namespace BethanysPieShop.ViewModels
     public class PiesListViewModel
     {
         public IEnumerable<Pie> Pies { get; set; }
+        public Pie Pie { get; set; }
         public string CurrentCategory { get; set; }
         public int CategoryId { get; set; }
         public IEnumerable<Category> Categories { get; set; }
